3f6c46e4170a2bc6228066b80a9aff87970f9c97
sxc6721 <brocksummers@hotmail.co.nz>
2022-08-25 17:49:47 +1200

    Brock-Dev-Commit2
    Added basis of infinite runner, generation and basic scene.

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    //variables
+    public float moveSpeed = 3;
+    public float leftRightSpeed = 4;
+
+    //updates every frame
+    void Update()
+    {
+        //Moves Forward indefinitely
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+        
+        //Basic Left movement on keyboard
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if(this.gameObject.transform.position.x > LevelBoundary.leftSide)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+            }
+        }
+
+        //Basic Right movement on keyboard
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if(this.gameObject.transform.position.x < LevelBoundary.rightSide)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+            }
+        }
+    }
+}
