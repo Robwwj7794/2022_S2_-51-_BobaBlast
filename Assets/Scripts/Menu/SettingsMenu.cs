8c6ebb9ac896f0b000062f5faca79ac7b1c14e14
RileyLeydesdorff <rfg9418@autuni.ac.nz>
2022-09-09 15:42:34 +1200

    Settings Menu
    Created initial implementation of the settings menu

---
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+    //public Toggle fullscreenTog;
+
+    public List<Res> resolutions = new List<Res>();
+    private int selectedResolution;
+
+    public TMP_Text resolutionLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //fullscreenTog.isOn = Screen.fullScreen;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ApplyGraphics()
+    {
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, Screen.fullScreen);
+    }
+
+    public void ResLeft()
+    {
+        selectedResolution--;
+        if(selectedResolution < 0)
+        {
+            selectedResolution = 0;
+        }
+
+        UpdateResLabel();
+    }
+
+    public void ResRight()
+    {
+        selectedResolution++;
+        if (selectedResolution > resolutions.Count - 1)
+        {
+            selectedResolution = resolutions.Count - 1;
+        }
+
+        UpdateResLabel();
+    }
+
+    public void UpdateResLabel()
+    {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " x " + resolutions[selectedResolution].vertical.ToString();    }
+    }
+
+[System.Serializable]
+public class Res
+{
+    public int horizontal, vertical;
+}
