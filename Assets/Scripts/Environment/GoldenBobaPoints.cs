92c2c27410111baad722f0aac0ed5208ae4b7b20
KT5334 <yfq5334@autuni.ac.nz>
2022-09-20 17:28:22 +1200

    Created gold and strawberry boba

---
+++ Assets/Scripts/Environment/GoldenBobaPoints.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldenBobaPoints : MonoBehaviour
+{
+    private ScoreManager scoreManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreManager = GameObject.Find("Canvas").GetComponent<ScoreManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //when the player touches the boba destroy it and increment score by 1. 
+        if (other.gameObject.tag == "Player")
+        {
+            scoreManager.score += 20;
+            //update lastScore field to be the same as score
+            scoreManager.lastScore = scoreManager.score;
+            Destroy(gameObject);
+        }
+    }
+}
