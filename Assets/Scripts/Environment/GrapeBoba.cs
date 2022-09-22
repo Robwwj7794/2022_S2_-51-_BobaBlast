3d4ede39a19d643b53482a215db3125f8a6aede8
KT5334 <yfq5334@autuni.ac.nz>
2022-09-22 18:03:07 +1200

    Created coconut and grape boba. Also created 3 extra sections

--- Assets/Scripts/Environment/GrapeBoba.cs
+++ Assets/Scripts/Environment/GrapeBoba.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StrawberryBobaPoints : MonoBehaviour
+public class GrapeBoba : MonoBehaviour
 {
     private ScoreManager scoreManager;
     // Start is called before the first frame update
@@ -22,7 +22,7 @@ public class StrawberryBobaPoints : MonoBehaviour
         //when the player touches the boba destroy it and increment score by 1. 
         if (other.gameObject.tag == "Player")
         {
-            scoreManager.score += 15;
+            scoreManager.score += 10;
             //update lastScore field to be the same as score
             scoreManager.lastScore = scoreManager.score;
             Destroy(gameObject);
